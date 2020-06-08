82613a32b533b560e74a79d15207317403f0bab2
thomas <iam@tangkunyin.com>
2020-06-08 18:04:37 +0800

    update mysql
    Signed-off-by: thomas <iam@tangkunyin.com>

--- utils/config.php
+++ utils/config.php
@@ -1,22 +1,29 @@
 <?php
 
-function get_db_con(){
+function get_db_con()
+{
+
+    $isDev = true;
 
     // MySql 数据库链接配置信息
     $serverName = "127.0.0.1";
-    $userName = "weixinsql";
-    $passWord = "dFNjn3JCDd~FNqjnq6v3qJCD";
     $port = 3306;
-    $dbName = "weixinsql";
 
+    // 以下分别是数据库用户名、密码、数据库名
+    $userName = "";
+    $passWord = "";
+    $dbName = "";
 
+    if ($isDev) {
+        $userName = "root";
+        $passWord = "root";
+        $dbName = "weixin";
+    }
 
-    $conn = mysqli_connect($serverName,$userName,$passWord,$dbName,$port);
-    if($conn->connect_errno){
-        die("数据库连接失败：".$conn->connect_errno);
+    $conn = mysqli_connect($serverName, $userName, $passWord, $dbName, $port);
+    if ($conn->connect_errno) {
+        die("数据库连接失败：" . $conn->connect_errno);
         return null;
     }
     return $conn;
-
 }
-?>
--- wx-core.js
+++ wx-core.js
@@ -1,30 +1,33 @@
-$().ready(function(){
-    getWeixinInfo();
+$ ().ready (function () {
+  getWeixinInfo ();
 });
 
-function getWeixinInfo(id){
-    var hasNoId = typeof id == 'undefined' || $.trim(id).length == 0;
-    $.get(hasNoId ? 'http://weixin.hujiakang.com/get.php' : 'http://weixin.hujiakang.com/get.php?wid='+id,null,function(data){
-        var jsonData = JSON.parse(data);
-        if(jsonData.code == 0){
-            var wxNum = jsonData.data.wxNum;
-            /*
+function getWeixinInfo (id) {
+  var hasNoId = typeof id == 'undefined' || $.trim (id).length == 0;
+  $.get (hasNoId ? '/get.php' : '/get.php?wid=' + id, null, function (data) {
+    var jsonData;
+    try {
+      jsonData = JSON.parse (data);
+      if (jsonData.code == 0) {
+        var wxNum = jsonData.data.wxNum;
+        /*
              在你的html中自定义一个元素，id为wxNumTextContainer。
              调用getwx.js后，将会在指定容器内显示并统计该微信号码
              */
-            $(".wxNumTextContainer").html(wxNum);
-            if(hasNoId){
-                doAnalysis(jsonData.data.wxId);
-            }
-        }else{
-            console.log(jsonData.msg);
+        $ ('.wxNumTextContainer').html (wxNum);
+        if (hasNoId) {
+          doAnalysis (jsonData.data.wxId);
         }
-    });
+      }
+    } catch (e) {
+      console.error (jsonData);
+    }
+  });
 }
 
-function doAnalysis(id){
-    $.get('http://weixin.hujiakang.com/get.php?wid='+id,null,function(data){
-        var jsonData = JSON.parse(data);
-        console.log(jsonData.code == 0 ? '有效统计' : '无效统计');
-    });
+function doAnalysis (id) {
+  $.get ('/get.php?wid=' + id, null, function (data) {
+    var jsonData = JSON.parse (data);
+    console.log (jsonData.code == 0 ? '有效统计' : '无效统计');
+  });
 }
